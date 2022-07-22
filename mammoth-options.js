05b3cb3b20dcb1dc046c963e19b06aaac5307cf9
Stéphane Gully <gully@abes.fr>
2022-07-22 13:17:57 +0200

    feat: complète avec tous les styles non gérés issus du document "11.2.2.4 Enregistrement du nom privilégié.docx"

--- mammoth-options.js
+++ mammoth-options.js
@@ -1,19 +1,6 @@
 function MAMMOTH_OPTIONS(mammoth) {
-    var styleMap = [
-		"p[style-name='rdaExemple5'] => p.rdaExemple5:fresh",
-        /*
-		"p[style-name='Custom Heading 1'] => h1",
-        "p[style-name='Custom Heading 2'] => h2",
 
-        "r[style-name='Custom Bold'] => strong",
-        "r[style-name='Custom Italic'] => em",
-
-        "p[style-name='Code Block C'] => pre.language-c:separator('\\n')",
-        "p[style-name='Code Block JavaScript'] => pre.language-javascript:separator('\\n')",
-        "p[style-name='Code Block'] => pre:separator('\\n')",
-		*/
-    ];
-
+    /*
     function setParagraphStyleNameFromFonts(fonts, styleName, skipStyleNameRegex) {
         return mammoth.transforms.paragraph(function(paragraph) {
             var runs = mammoth.transforms.getDescendantsOfType(paragraph, "run");
@@ -27,18 +14,30 @@ function MAMMOTH_OPTIONS(mammoth) {
             }
         });
     }
+    */
 
 
     return {
-        styleMap: styleMap,
-        // If a paragraph is entirely made up of runs that use monospace fonts,
-        // update the paragraph to use the style "Code Block" if it doesn't
-        // already have a "Code Block" style.
+        // ici sont listé les sites des docx non standard à Word
+        // pour les mapper sur des classes CSS
+        styleMap: [
+            "p[style-name='rdaExemple5'] => p.rdaExemple5:fresh",
+            "p[style-name='rdaAlternative/exception/ajout'] => span.rdaAlternativeexceptionajout:fresh",
+            "r[style-name='Note de bas de page Car'] => span.NotedebasdepageCar:fresh",
+            "p[style-name='Normal (Web)'] => span.NormalWeb:fresh",
+        ],
+
+/*
         transformDocument: setParagraphStyleNameFromFonts(
             ["consolas", "courier", "courier new"],
             "Code Block",
             /^Code Block/i
         ),
+*/
+        // ce code permet de transformer les images en base64 pour éviter qu'elles apparaissent
+        // en nombre dans la médiathèque de wordpress (cf petite image RDA très nombreuses dans les documents)
+        // cette façon de faire n'est pas très optimisée d'une point de vue HTML car les pages générées sont alors 
+        // volumineuses mais c'est actuellement le meilleur moyen trouvé pour éviter de polluer la médiathèque de wordpress
 		convertImage: mammoth.images.imgElement(function(image) {
 			return image.read("base64").then(function(imageBuffer) {
 				return {
